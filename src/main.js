95008424b3724ac781ea448475d4aa57ff959195
jordan <i.aslanoglou@gazzetta.gr>
2025-02-25 16:22:08 +0200

    header-animations

--- src/main.js
+++ src/main.js
@@ -1,3 +1,51 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+    const body = document.body;
+    const toggleButton = document.querySelector(
+        '[data-collapse-toggle="navbar-sticky"]',
+    );
+    const navbarMenu = document.getElementById("navbar-sticky");
+
+    toggleButton.addEventListener("click", () => {
+        navbarMenu.classList.toggle("active");
+        toggleButton.classList.toggle("active");
+    });
+
+    let timeout;
+    window.addEventListener("scroll", () => {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => {
+            body.setAttribute('data-scrolled', window.scrollY > 25);
+        }, 100);
+    });
+
+    window.addEventListener("resize", () => {
+        navbarMenu.classList.remove("active");
+        toggleButton.classList.remove("active");
+    });
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                const element = entry.target;
+                const animationClass = element.dataset.animation;
+                element.classList.add(animationClass);
+                observer.unobserve(element); // Remove observer after animating once
+            }
+        });
+    }, {
+        threshold: 0.1, // Trigger when 10% of the element is visible
+    });
+
+    // Observe each section with data-animation attribute
+    document.querySelectorAll("[data-animation]").forEach((el) => {
+        observer.observe(el);
+    });
+
+    const blackButtons = document.querySelectorAll('.btn-black');
+    blackButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            button.classList.toggle('btn-black--outline');
+        });
+    });
 });
